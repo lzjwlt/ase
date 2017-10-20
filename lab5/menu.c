ada9c3dd51596f596e561288014b728bdd2cbc76
lzjustc <ustclzj@foxmail.com>
2017-10-20 17:03:20 +0800

    add lab5 firsttime

---
+++ lab5/menu.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "linktable.h"
+
+void Help();
+void Version();
+void Quit();
+void Hello();
+void Time();
+void Author();
+void Triangle();
+void Heart();
+
+#define CMD_MAX_LEN 128
+#define DESC_LEN    1024
+#define CMD_NUM     10
+
+char cmd[CMD_MAX_LEN]; 
+
+typedef struct DataNode
+{
+    tLinkTableNode* pNext;
+    char* cmd;
+    char* desc;
+    void (*handler)();
+}tDataNode;
+
+int SearchCondition(tLinkTableNode * pLinkTableNode)
+{
+    tDataNode * pNode = (tDataNode *)pLinkTableNode;
+    if(strcmp(pNode->cmd, cmd) == 0)
+    {
+        return  SUCCESS;  
+    }
+    return FAILURE;	       
+}
+
+tDataNode* FindCmd(tLinkTable* head, char* cmd)
+{
+    return  (tDataNode*)SearchLinkTableNode(head,SearchCondition);
+}
+
+int ShowAllCmd(tLinkTable* head)
+{
+    tDataNode* pNode = (tDataNode*) GetLinkTableHead(head);
+    while(pNode != NULL)
+    {
+        printf("%10s   -----   %s\n", pNode->cmd, pNode->desc);
+        pNode = (tDataNode*)GetNextLinkTableNode(head, (tLinkTableNode*)pNode);
+    }
+    return 0;
+}
+
+typedef struct CMDNode
+{
+    char* cmd;
+    char* desc;
+    void (*handler)();
+    struct CMDNode *next;
+}tCMDNode;
+    
+tCMDNode Head[] =
+{
+    {"help", "This is help command.", Help, &Head[1]},
+    {"version","Show version of this menu program.", Version, &Head[2]},
+    {"quit", "Quit and back to OS.", Quit, &Head[3]},
+    {"hello", "Say hello to users.", Hello, &Head[4]},
+    {"time", "Show system date and time.", Time, &Head[5]},
+    {"author", "Show author information.", Author, &Head[6]},
+    {"triangle", "Show a big triangle on screen.", Triangle, &Head[7]},
+    {"heart", "Show a big heart on screen.", Heart, NULL},
+};
+
+tLinkTable* InitMenuData(tCMDNode* Head, int length)
+{
+    tLinkTable* pLinkTable = CreateLinkTable();
+    int i;
+    for(i=0; pLinkTable->sumOfNode < length; i++)
+    {
+        tDataNode* pNode = (tDataNode*)malloc(sizeof(tDataNode));
+        pNode->cmd = Head[i].cmd;
+        pNode->desc = Head[i].desc;
+        pNode->handler = Head[i].handler;
+        AddLinkTableNode(pLinkTable, (tLinkTableNode*)pNode);
+    }
+    return pLinkTable;
+}
+
+int main()
+{
+	tLinkTable* head = InitMenuData(Head, 8); 
+    while(1)
+    {
+        printf("menu cmd-> ");
+        scanf("%s", cmd);
+        tDataNode* p = FindCmd(head, cmd);      
+        if(p == NULL)
+        {
+            printf("error: Wrong command!\n");
+        }
+        else
+        {
+            p->handler();
+        }
+    }
+    return 0;
+}
+
+void Help()
+{
+    printf("*************************************************************\n");
+    printf("This is help command.\n\n");
+    printf("commands:\n");
+	tLinkTable* head = InitMenuData(Head, 8); 
+    ShowAllCmd(head);
+    printf("*************************************************************\n");
+}
+
+void Version()
+{
+    printf("menu version v2.8\n");
+}
+
+void Quit()
+{
+    printf("Goodbye\n");
+    exit(0);
+}
+
+void Hello()
+{
+    printf("Hi, How are you?\n");
+    char answer;
+    printf("Input (Y/N), Y:I'm fine, thank you!  N:I'm feeling terrible\n");
+    getchar();
+    answer = getchar();
+    if(answer == 'Y' || answer == 'y')
+    {
+        printf("Wish you happy everyday!\n");
+    }
+    else if(answer == 'N' || answer == 'n')
+    {
+        printf("Cheer up, things will work out for the best!\n");
+    }
+    else
+    {
+        printf("Sorry, I can't understand it!\n");
+    }
+}
+
+void Time()
+{
+    system("date");
+}
+
+void Author()
+{
+    printf("*************************************************************\n");
+    printf("Author infomation\n\n");
+    printf("%12s :  Li Zhijun (厉治军)\n", "name");
+    printf("%12s :  SA17225184\n", "Student ID");
+    printf("%12s :  软设2班\n", "Class");
+    printf("School of software engineering, USTC\n");
+    printf("*************************************************************\n");
+}
+
+void Triangle()
+{
+    int i,j;
+    int n = 20;
+    for(i = 0; i < n; i++)
+    {
+        for(j = n - i + 10; j > 0; j--)
+        {
+            putchar(' ');
+        }
+        for(j = 0; j < i * 2 - 1; j++)
+        {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+}
+
+void Heart()
+{
+    float x, y;
+    for(y = 1.5f; y > -1.5f; y -= 0.1f)
+    {
+        for(x = -1.5f; x < 1.5f; x += 0.05f)
+        {
+            float a = x * x + y * y - 1;
+            putchar(a * a * a - x * x * y * y * y <= 0.0f ? '*': ' ');
+        }
+        putchar('\n');
+    }
+
+}
+
